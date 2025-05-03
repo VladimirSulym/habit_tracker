7ebef198c1c919ede0acc2b7d9724cbc309a0bf9
Vladimir Sulym <vormagic@yandex.ru>
2025-05-03 15:30:58 +0300

    my first commit

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Электронная почта")
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = [
+            "email",
+        ]
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
